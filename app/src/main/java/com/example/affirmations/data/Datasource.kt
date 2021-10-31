0554da7b1bc100cc332577834789ee8e68dd190c
SpencerDamon <spencerdamon1974@gmail.com>
2021-10-30 21:21:49 -0700

    Created 2 new projects, and 2 classes for data
    Created new project .model and class Affirmation.kt
    Created Affirmation data class Affirmation
    Passed in value stringResource as type Integer
    This removes the data error requiring one property
    Created new project .data and class Datasource.kt
    Created new class Datasource and loadAffirmation fun
    Defined the output as List<Affirmation>
    Return and create list with type Affirmation
    Populated the list with 10 affirmations from strings

---
+++ app/src/main/java/com/example/affirmations/data/Datasource.kt
@@ -0,0 +1,45 @@
+/**
+ * Data displayed may come from different sources (eg. within your app project, or an online source.
+ * As a result the data may not be the exact format you need. The rest of the app should not concern
+ * itself with where it came from, or what was it's original format.
+ * You should hide it away in a seperate Datasource class that prepares the data.
+ */
+/**
+ * Create a package in the package that holds MainActivity, name it data
+ *  Append the parent package name with .data
+ *  In the new package create a class called Datasource, this creates a Datasource.kt file
+ */
+package com.example.affirmations.data
+
+import com.example.affirmations.R
+import com.example.affirmations.model.Affirmation
+
+class Datasource {
+    /** Create a function called loadAffirmations, which needs to return a list of Affirmations.
+     *  You do this by creating a list, and populating it with an Affirmation instance for each
+     *  resource string.
+     *  Declare List<Affirmation> as the return type
+     *  Call listOf<>() to create a List
+     *  Inside the angle brackets specify the type of the list items as Affirmation
+     *  If necessary (Auto Import is not on in settings),
+     *  import com.example.affirmations.model.Affirmation
+     *  Inside the parantheses, create an Affirmation, passing in R.string.affirmation1
+     *  as the resource ID.
+     *  Add the remaining affirmations, seperated by a comma , .
+     */
+    fun loadAffirmations(): List<Affirmation>{
+        return listOf<Affirmation>(
+            Affirmation(R.string.affirmation1),
+            Affirmation(R.string.affirmation2),
+            Affirmation(R.string.affirmation3),
+            Affirmation(R.string.affirmation4),
+            Affirmation(R.string.affirmation5),
+            Affirmation(R.string.affirmation6),
+            Affirmation(R.string.affirmation7),
+            Affirmation(R.string.affirmation8),
+            Affirmation(R.string.affirmation9),
+            Affirmation(R.string.affirmation10)
+
+        )
+    }
+}
---
+++ app/src/main/java/com/example/affirmations/model/Affirmation.kt
@@ -0,0 +1,10 @@
+package com.example.affirmations.model
+
+// When you make it a data class, you must add at least one property.
+data class Affirmation(val stringResource: Int) {
+    /** When you create an instance of Affirmation, you need to pass in the resource ID for the
+     *  affirmation string.  The resource ID is an integer. This satisfies the issues giving the
+     *  error when data was added to the class keyword.
+     */
+
+}
