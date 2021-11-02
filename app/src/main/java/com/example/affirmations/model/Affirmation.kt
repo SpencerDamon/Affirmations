054390086387aa00390b00c34f55e58e97ac0201
SpencerDamon <spencerdamon1974@gmail.com>
2021-11-01 20:06:09 -0700

    Added documentation
    Added documentation about the need to pass in the
    resource ID for the affirmation string as an Int
    to satisfy error.

--- app/src/main/java/com/example/affirmations/model/Affirmation.kt
+++ app/src/main/java/com/example/affirmations/model/Affirmation.kt
@@ -1,7 +1,7 @@
 package com.example.affirmations.model
 
 // When you make it a data class, you must add at least one property.
-data class Affirmation(val stringResource: Int) {
+data class Affirmation(val stringResourceId: Int) {
     /** When you create an instance of Affirmation, you need to pass in the resource ID for the
      *  affirmation string.  The resource ID is an integer. This satisfies the issues giving the
      *  error when data was added to the class keyword.
