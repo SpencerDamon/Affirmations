c0d9d25bcd1010097703d241a7672bf6ef17ded5
SpencerDamon <spencerdamon1974@gmail.com>
2021-11-08 00:17:23 -0800

    Updated Affirmation class param
    Added a second property to the Affirmation class, image resource id as an Int type.
    Added param annotation @StringRes.
    Added param annotation @DrawableRes.
    Imported param dependencies

--- app/src/main/java/com/example/affirmations/model/Affirmation.kt
+++ app/src/main/java/com/example/affirmations/model/Affirmation.kt
@@ -1,7 +1,12 @@
 package com.example.affirmations.model
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
 // When you make it a data class, you must add at least one property.
-data class Affirmation(val stringResourceId: Int) {
+data class Affirmation(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResoureId: Int) {
     /** When you create an instance of Affirmation, you need to pass in the resource ID for the
      *  affirmation string.  The resource ID is an integer. This satisfies the issues giving the
      *  error when data was added to the class keyword.
