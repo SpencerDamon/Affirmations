bcad688cb9ab6f262bea53336904208bacf95b74
SpencerDamon <spencerdamon1974@gmail.com>
2021-11-08 00:22:22 -0800

    Added drawables to Affirmation List
    Passed in 10 resource id's as an argument for each Affirmation instance that has been initialized.

--- app/src/main/java/com/example/affirmations/data/Datasource.kt
+++ app/src/main/java/com/example/affirmations/data/Datasource.kt
@@ -7,9 +7,9 @@
 /**
  * Since preparing data is a separate concern in regards to the app functionality, we can hide it
  * away in a separate data source class in a separate data package.
- * Create a package in the package that holds MainActivity, name it data
- *  Append the parent package name with .data
- *  In the new package create a class called Datasource, this creates a Datasource.kt file
+ * 1. Create a package in the package that holds MainActivity, name it data
+ *    Append the parent package name with .data
+ *    In the new package create a class called Datasource, this creates a Datasource.kt file
  */
 package com.example.affirmations.data
 
@@ -17,30 +17,30 @@ import com.example.affirmations.R
 import com.example.affirmations.model.Affirmation
 
 class Datasource {
-    /** Create a function called loadAffirmations, which needs to return a list of Affirmations.
-     *  You do this by creating a list, and populating it with an Affirmation instance for each
-     *  resource string.
-     *  Declare List<Affirmation> as the return type
-     *  Call listOf<>() to create a List
-     *  Inside the angle brackets specify the type of the list items as Affirmation
-     *  If necessary (Auto Import is not on in settings),
-     *  import com.example.affirmations.model.Affirmation
-     *  Inside the parentheses, create an Affirmation, passing in R.string.affirmation1
-     *  as the resource ID.
-     *  Add the remaining affirmations, separated by a comma , .
+    /** 2. Create a function called loadAffirmations, which needs to return a list of Affirmations.
+     *     You do this by creating a list, and populating it with an Affirmation instance for each
+     *     resource string.
+     *     Declare List<Affirmation> as the return type
+     *     Call listOf<>() to create a List
+     *     Inside the angle brackets specify the type of the list items as Affirmation
+     *     If necessary (Auto Import is not on in settings),
+     *     import com.example.affirmations.model.Affirmation
+     *     Inside the parentheses, create an Affirmation, passing in R.string.affirmation1
+     *     as the resource ID.
+     *     Add the remaining affirmations, separated by a comma , .
      */
     fun loadAffirmations(): List<Affirmation>{
         return listOf<Affirmation>(
-            Affirmation(R.string.affirmation1),
-            Affirmation(R.string.affirmation2),
-            Affirmation(R.string.affirmation3),
-            Affirmation(R.string.affirmation4),
-            Affirmation(R.string.affirmation5),
-            Affirmation(R.string.affirmation6),
-            Affirmation(R.string.affirmation7),
-            Affirmation(R.string.affirmation8),
-            Affirmation(R.string.affirmation9),
-            Affirmation(R.string.affirmation10)
+            Affirmation(R.string.affirmation1, R.drawable.image1),
+            Affirmation(R.string.affirmation2, R.drawable.image2),
+            Affirmation(R.string.affirmation3, R.drawable.image3),
+            Affirmation(R.string.affirmation4, R.drawable.image4),
+            Affirmation(R.string.affirmation5, R.drawable.image5),
+            Affirmation(R.string.affirmation6, R.drawable.image6),
+            Affirmation(R.string.affirmation7, R.drawable.image7),
+            Affirmation(R.string.affirmation8, R.drawable.image8),
+            Affirmation(R.string.affirmation9, R.drawable.image9),
+            Affirmation(R.string.affirmation10, R.drawable.image10)
 
         )
     }
