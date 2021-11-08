908218392fa57d7de199c681922c737eb3dd18e0
SpencerDamon <spencerdamon1974@gmail.com>
2021-11-08 14:49:28 -0800

    Fixed misspelled variable
    Fixed misspelled variable imageResourceId

--- app/src/main/java/com/example/affirmations/model/Affirmation.kt
+++ app/src/main/java/com/example/affirmations/model/Affirmation.kt
@@ -6,7 +6,7 @@ import androidx.annotation.StringRes
 // When you make it a data class, you must add at least one property.
 data class Affirmation(
     @StringRes val stringResourceId: Int,
-    @DrawableRes val imageResoureId: Int) {
+    @DrawableRes val imageResourceId: Int) {
     /** When you create an instance of Affirmation, you need to pass in the resource ID for the
      *  affirmation string.  The resource ID is an integer. This satisfies the issues giving the
      *  error when data was added to the class keyword.
